d9ffd96cf6d0c16e7565d5f82619be3b62114235
p2635 <4522927+p2635@users.noreply.github.com>
2023-02-09 14:20:37 +0000

    Add app code

---
+++ 03-TimeConversionApp/TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Phil on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
