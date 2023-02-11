7fcd1bf258eaecd333638a89e359e4291f59ec32
p2635 <4522927+p2635@users.noreply.github.com>
2023-02-11 16:03:14 +0000

    Complete guess the flag game

---
+++ 04-GuessTheFlag/Guess The Flag/StacksPractice.swift
@@ -0,0 +1,20 @@
+//
+//  StacksPractice.swift
+//  Guess The Flag
+//
+//  Created by Phil on 10/02/2023.
+//
+
+import SwiftUI
+
+struct StacksPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StacksPractice_Previews: PreviewProvider {
+    static var previews: some View {
+        StacksPractice()
+    }
+}
